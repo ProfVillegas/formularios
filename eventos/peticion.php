31fe4d3ec0009a7b284321bc40edd0bd028ba2f9
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2017-06-20 13:46:26 -0500

    Merge branch 'master' of https://github.com/ProfVillegas/formularios
    ajax

--- eventos/peticion.php
+++ eventos/peticion.php
@@ -1,4 +1,10 @@
 <?php
+<<<<<<< HEAD
 	echo '<p id="nombre">'.$_GET['nombre']."</p>";
 	echo '<p id="edad">'.$_GET['edad']."</p>";
+=======
+echo '<p id="nombre">'.$_GET['nombre']."</p>";
+echo '<p id="edad">'.$_GET['edad']."</p>";
+
+>>>>>>> 0534e15984973aa1eb634ab52781881daf4f15a2
 ?>
