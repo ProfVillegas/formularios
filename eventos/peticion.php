f302a4073afd392f8fecbaa7bf28e9ac6d7d11f0
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2017-06-21 11:08:10 -0500

    changes
    des

--- eventos/peticion.php
+++ eventos/peticion.php
@@ -1,7 +1,13 @@
 <?php
 <<<<<<< HEAD
+<<<<<<< HEAD
 	echo '<p id="nombre">'.$_GET['nombre']."</p>";
 	echo '<p id="edad">'.$_GET['edad']."</p>";
+=======
+echo '<p id="nombre">'.$_GET['nombre']."</p>";
+echo '<p id="edad">'.$_GET['edad']."</p>";
+
+>>>>>>> 0534e15984973aa1eb634ab52781881daf4f15a2
 =======
 echo '<p id="nombre">'.$_GET['nombre']."</p>";
 echo '<p id="edad">'.$_GET['edad']."</p>";
